6812b2ca568a3e32a8253952b139a1ec93c0ed90
aadwilli <aadwilli15@gmail.com>
2017-08-03 13:50:44 -0400

    basic scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth'
 
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { routes } from './services/routes';
+import { firebaseConfig } from '../environments/firebase.config';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    AdminComponent,
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+@Injectable()
+export class FirebaseService{
+    fbs = this;
+    authState;
+
+    // signup(user: User){
+    //     this.af.auth.createUserWithEmailAndPassword(user.email,user.password)
+    //     .then(() => {
+    //         this.router.navigateByUrl('/myprofile');
+    //     })
+    //     .catch((e) => {
+    //         console.log(e);
+    //     })
+    //     console.log(this.isAuthed());
+    // }
+    signin(user: User){
+        this.af.auth.signInWithEmailAndPassword(user.email, user.password)
+        .then(() => {
+            this.router.navigateByUrl('/myprofile');
+        })
+        .catch((e) => {
+            console.log(e);
+        })
+    }
+    isAuthed(){
+        return !!this.authState;
+    }
+    constructor(private af: AngularFireAuth, private router: Router){
+        this.af.authState.subscribe((authState) => {
+            this.authState = authState
+        })
+    }
+}

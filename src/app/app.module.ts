66ba4054f0500077b30b4d6312958303b08fd73c
Harrison Schultz <hschultz1337@gmail.com>
2017-08-03 11:10:47 -0400

    added bootstrap and full calendar to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Modules
+import { BrowserModule } from '@angular/platform-browser';
+import { CalendarModule } from "ap-angular2-fullcalendar";
+
+//Components
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]

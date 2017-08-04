1146008cfea8dbb5a1098657302225691c0e386c
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-08-04 09:45:03 -0400

    student signup html added to admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FirebaseService } from '../services/auth.service';
+
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent implements OnInit {
 
-  constructor() { }
+// TODO: Add the firebase rejections to validate
+export class AdminComponent {
+  public errors = {email: '', pass: '', confpass: ''}; // This one is public so that angular can access it
+  model = {email: '', pass: '', confpass: ''}; // Model that angular will store data in
+  user: User; // User that we will send to the database
+  fbs: FirebaseService;
 
-  ngOnInit() {
+  validate() {
+    this.errors = {email: '', pass: '', confpass: ''};
+    if (!this.model.email) {
+      this.errors.email = 'Please provide a username';
+    }
+    if (!this.model.pass) {
+      this.errors.pass = 'Please provide a password';
+    }
+    if (!this.model.confpass) {
+      this.errors.confpass = 'Please confirm your password';
+    }
+    if (this.model.confpass !== this.model.pass) {
+      this.errors.confpass = 'Passwords must match';
+    }
+    return(this.errors.email || this.errors.pass || this.errors.confpass); // Returns true if there are errors
   }
 
+  onSubmit() {
+    if (this.validate()) {  // If there are errors, do not submit the form
+      return;
+    }
+    this.user = new User(this.model.email, this.model.pass); // Create a new user object with the model email and password
+    console.log(this.user);
+    // Pass the user to the db here
+    this.fbs.signup(this.user);
+    console.log(this.fbs.isAuthed());
+  }
+
+  constructor(private firebase: FirebaseService){
+    this.fbs = firebase;
+    console.log(this.fbs.isAuthed());
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,35 +5,36 @@ import { Router } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable()
-export class FirebaseService{
+export class FirebaseService {
     fbs = this;
     authState;
 
-    // signup(user: User){
-    //     this.af.auth.createUserWithEmailAndPassword(user.email,user.password)
-    //     .then(() => {
-    //         this.router.navigateByUrl('/myprofile');
-    //     })
-    //     .catch((e) => {
-    //         console.log(e);
-    //     })
-    //     console.log(this.isAuthed());
-    // }
-    signin(user: User){
-        this.af.auth.signInWithEmailAndPassword(user.email, user.password)
+    signup(user: User) {
+        this.af.auth.createUserWithEmailAndPassword(user.email,user.password)
         .then(() => {
-            this.router.navigateByUrl('/myprofile');
+            this.router.navigateByUrl('/admin');
         })
         .catch((e) => {
             console.log(e);
-        })
+        });
+        console.log(this.isAuthed());
     }
-    isAuthed(){
+
+    signin(user: User) {
+        this.af.auth.signInWithEmailAndPassword(user.email, user.password)
+        .then(() => {
+            this.router.navigateByUrl('/student');
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+    }
+    isAuthed() {
         return !!this.authState;
     }
     constructor(private af: AngularFireAuth, private router: Router){
         this.af.authState.subscribe((authState) => {
-            this.authState = authState
-        })
+            this.authState = authState;
+        });
     }
 }

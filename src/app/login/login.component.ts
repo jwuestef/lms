e2e55a41ad8ba7d0262d54f2c4e4d4a35878ca97
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-08-04 15:44:03 -0400

    login and logout working, missing styling

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/auth.service';
+
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  public errors = {email: '', pass: ''};  // This one is public so that angular can access it
+  model = {email: '', pass: ''};  // Model that angular will store data in
+  user: User; // User that we will send to the database
+  fbs: FirebaseService;
 
-  constructor() { }
+  validate() {
+    this.errors = {email: '', pass: ''};
+    if (!this.model.email) {
+      this.errors.email = 'Please provide an email';
+    }
+    if (!this.model.pass) {
+      this.errors.pass = 'Please provide a password';
+    }
+    return(this.errors.email || this.errors.pass);  // Returns true if there are errors
+  }
 
-  ngOnInit() {
+  onSubmit() {
+    if (this.validate()) { // If there are errors, do not submit the form
+      return;
+    }
+    this.user = new User(this.model.email, this.model.pass);
+    this.fbs.signin(this.user);
+    localStorage.setItem('userEmail', this.user.email);
+  }
+
+  constructor(private firebase: FirebaseService) {
+    this.fbs = firebase;
   }
 
 }

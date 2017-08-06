85b41aed4b5de84cde9ca1cb6032206cc3202b27
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-08-06 14:01:13 -0400

    added /admin route protection

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireDatabase } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-admin',
@@ -7,7 +10,20 @@ import { Component } from '@angular/core';
 })
 
 export class AdminComponent {
-  constructor() {
+
+  constructor(public router: Router, public afd: AngularFireDatabase) {
+        const thisSaved = this;
+        this.afd.database.ref('/isAdmin').once('value').then(function(isAdminTable) {
+          const arrayOfAdmins = isAdminTable.val();
+          const userEmail = localStorage.getItem('userEmail');
+          const atSign = userEmail.search('@');
+          const userToCheckIfAdmin = userEmail.slice(0, atSign);
+          const isAdmin = arrayOfAdmins.hasOwnProperty(userToCheckIfAdmin);
+          if (!isAdmin) {
+            thisSaved.router.navigateByUrl('/student');
+          }
+        });
   }
+
 
 }

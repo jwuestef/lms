0925d8f492d50c0d51ee5a964e3447506c84661f
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-08-08 15:57:14 -0400

    pre - npmFlashMessageInstallation

--- src/app/student-management/student-management.component.ts
+++ src/app/student-management/student-management.component.ts
@@ -2,19 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { NgModel } from '@angular/forms';
 import { AngularFireDatabase } from 'angularfire2/database';
 
+import { EventService } from '../services/event.service';
+
+
 @Component({
   selector: 'app-student-management',
   templateUrl: './student-management.component.html',
   styleUrls: ['./student-management.component.css']
 })
 export class StudentManagementComponent {
+  newStudentUsername = '';
 
-  constructor() { }
 
-// addStudent(){
-//   const currentStudent{
-//     username: this.username
-//   }
-// }
+
+  constructor(public afd: AngularFireDatabase, public es: EventService) {
+    console.log('StudentManagementComponent loaded!');
+  }
+
+
+
+  addStudent() {
+    console.log('addStudent called with newStudentUsername:');
+    console.log(this.newStudentUsername);
+  }
+
+
+
 
 }

62b28ae63acc13f785546bd318c097aaca8ba75b
aadwilli <aadwilli15@gmail.com>
2017-08-08 15:16:49 -0400

    stuff

--- src/app/student-management/student-management.component.ts
+++ src/app/student-management/student-management.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-student-management',
   templateUrl: './student-management.component.html',
   styleUrls: ['./student-management.component.css']
 })
-export class StudentManagementComponent implements OnInit {
+export class StudentManagementComponent {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+// addStudent(){
+//   const currentStudent{
+//     username: this.username
+//   }
+// }
 
 }
